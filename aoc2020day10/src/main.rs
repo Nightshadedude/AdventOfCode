59839f733463aacbbe3ad918e46496dc019368d1
Eric Landeis <nightshadedude@gmail.com>
2021-02-07 22:36:45 -0700

    day 10 part 2

--- aoc2020day10/src/main.rs
+++ aoc2020day10/src/main.rs
@@ -1,16 +1,18 @@
 use std::fs;
+use std::collections::HashMap;
+
 
 struct AdapterArray {
-    adapters: Vec<u16>,
-    voltage_diff: Vec<u16>, //voltage diff
+    adapters: Vec<u64>,
+    voltage_diff: Vec<u64>, //voltage diff
 }
 
 impl AdapterArray {
     fn new(s: &str) -> Self {
         let mut spl = s.split_terminator("\n")
-            .map(|i| i.parse::<u16>()
+            .map(|i| i.parse::<u64>()
                  .expect("unable to parse"))
-            .collect::<Vec<u16>>();
+            .collect::<Vec<u64>>();
         spl.push(0);
         spl.sort();
         spl.push(spl.last().unwrap()+3);
@@ -37,6 +39,35 @@ impl AdapterArray {
     }
 }
 
+fn part2(
+    mtable: &mut HashMap<(u64, usize), u64>,
+    pval: u64,
+    idx: usize,
+    nums: &[u64],
+    ) -> u64 {
+    if let Some(res) = mtable.get(&(pval, idx)) {
+        return *res;
+    }
+    let slice = &nums[idx..nums.len()];
+    if slice[0] - pval > 3 {
+        return 0;
+    }
+
+    if slice.len() == 1 {
+        return 1;
+    }
+    let w_next =
+        part2(mtable, slice[0], idx + 1, nums);
+    let wo_next = if slice[1] - pval > 3 {
+        0
+    } else {
+        part2(mtable, pval, idx + 1, nums)
+    };
+    let res = w_next + wo_next;
+    mtable.insert((pval, idx), res);
+    res
+}
+
 
 
 fn main() {
@@ -45,4 +76,9 @@ fn main() {
     let mut adapter_array = AdapterArray::new(&contents);
     adapter_array.calc_diff();
     println!("answer: {}", adapter_array.get_answer());
+    let part_2 = part2(&mut HashMap::new(),
+        adapter_array.adapters[0],
+        1,
+        &adapter_array.adapters);
+    println!("{}", part_2);
 }
