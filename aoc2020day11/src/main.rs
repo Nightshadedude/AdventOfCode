7ec70acfe201cbddfb423e1712bf8fcea69a2f01
Eric Landeis <nightshadedude@gmail.com>
2021-02-10 20:50:10 -0700

    day 11 part 1

--- aoc2020day11/src/main.rs
+++ aoc2020day11/src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct SeatMap{
     layout: Vec<Vec<char>>,
-    next_layout: Vec<Vec<char>>,
+    //next_layout: Vec<Vec<char>>,
 }
 
 impl SeatMap{
@@ -22,14 +22,13 @@ impl SeatMap{
 
         SeatMap{
             layout,
-            next_layout: vec![vec![]],
         }
     }
 
     fn validate_seats(&mut self) {
         let x_max = self.layout.len();
         let y_max = self.layout[0].len();
-        let mut check: Vec<Vec<char>> = vec![vec![]];
+        let mut check: Vec<Vec<char>> = Vec::new();
         for x in 0..x_max {
             let mut row: Vec<char> = vec![];
             for y in 0..y_max {
@@ -37,71 +36,93 @@ impl SeatMap{
                 match seat {
                     'L' => {
                         let mut temp = 0;
-                        for x_offset in 0..2 {
-                            for y_offset in 0..2 {
+                        for x_offset in 0..=2 {
+                            for y_offset in 0..=2 {
                                 if x_offset == 1 && y_offset == 1 {}
                                 else {
                                     match self
                                         .layout[x+x_offset-1][y + y_offset-1]
-                                    {
-                                        '#' => {temp += 1;},
-                                        _ => (),
-                                    }
+                                        {
+                                            '#' => {temp += 1;},
+                                            _ => (),
+                                        }
                                 }
                             }
                         }
                         if temp > 0 {
-                            &row.push('L');
+                            row.push('L');
                         }
                         else {
-                            &row.push('#');
+                            row.push('#');
                         }
                     },
                     '#' => {
                         let mut temp = 0;
-                        for x_offset in 0..2 {
-                            for y_offset in 0..2 {
+                        for x_offset in 0..=2 {
+                            for y_offset in 0..=2 {
                                 if x_offset == 1 && y_offset == 1 {}
                                 else {
                                     match self
                                         .layout[x+x_offset-1][y + y_offset-1]
-                                    {
-                                        '#' => {temp += 1;},
-                                        _ => (),
-                                    }
+                                        {
+                                            '#' => {temp += 1;},
+                                            _ => (),
+                                        }
                                 }
                             }
                         }
                         if temp >= 4 {
-                            &row.push('L');
+                            row.push('L');
                         }
                         else {
-                            &row.push('#');
+                            row.push('#');
                         }
-                    
+
                     },
                     '.' => {
-                        &row.push('.');
+                        row.push('.');
                     },
                     _ => {
-                        println!("No idea: ({},{}) - {}",
+                        println!("Unsure how we got here, X: {}, Y: {}, Ch: {}",
                         x,
                         y,
                         self.layout[x][y]);
                     },
                 }
-            &check.push(row);
             }
-        self.layout = check;
-        self.next_layout = vec![vec![]];
+            check.push(row);
         }
+        self.layout = check;
+    }
+
+    fn let_er_rip(&mut self) -> u64 {
+        loop {
+            let prev = self.layout.clone();
+            self.validate_seats();
+            let next = self.layout.clone();
+            if prev == next {
+                break;
+            }
+        }
+        self.count('#')
+    }
+
+    fn count(&self, ch: char) -> u64 {
+        let mut count = 0;
+        for row in self.layout.iter() {
+            for layout_ch in row.iter() {
+                if layout_ch == &ch {
+                    count += 1;
+                }
+            }
+        }
+        count
     }
 }
 
 fn main() {
     let filename = "input";
     let contents = fs::read_to_string(filename).expect("failed to read");
-    let seats = SeatMap::new(contents);
-    println!("{:?}", seats);
+    let mut seats = SeatMap::new(contents);
+    println!("{}", seats.let_er_rip());
 }
-
