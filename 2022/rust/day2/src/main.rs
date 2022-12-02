27c7263e2f8d86ff6420f44683b727a0af378137
nightshadedude <nightshadedude@gmail.com>
2022-12-02 08:55:43 -0700

    part 2 complete

--- 2022/rust/day2/src/main.rs
+++ 2022/rust/day2/src/main.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::str::FromStr;
 
 fn read_file(name: &str) -> String {
     return fs::read_to_string(name)
@@ -11,73 +10,81 @@ enum Throw {
     Rock,
     Paper,
     Scissors,
-    Error
+    Error,
 }
 
+#[derive(Debug)]
+enum Outcome {
+    Win,
+    Draw,
+    Loss,
+    Error,
+}
 
-fn throw_value(throw: &Throw) -> i32 {
-    match throw {
-        &Throw::Rock => 1,
-        &Throw::Paper => 2,
-        &Throw::Scissors => 3,
-        _ => 0,
+#[derive(Debug)]
+struct Game {
+    throw: Throw,
+    outcome: Outcome,
+}
+
+impl Game {
+    fn new(v: Vec<&str>)-> Self {
+        let throw = match v[0] {
+            "A" => Throw::Rock,
+            "B" => Throw::Paper,
+            "C" => Throw::Scissors,
+            _ => Throw::Error,
+        };
+        let outcome = match v[1] {
+            "X" => Outcome::Loss,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => Outcome::Error
+        };
+
+        Self {
+            throw,
+            outcome,
+        }
+    }
+
+    fn compare(&self) -> i32 {
+        match &self.outcome {
+            &Outcome::Win => 6,
+            &Outcome::Draw => 3,
+            &Outcome::Loss => 0,
+            _ => 0,
+        }
+    }
+
+    fn throw_value(&self) -> i32 {
+        match (&self.outcome, &self.throw) {
+            (&Outcome::Win, &Throw::Rock) => 2,
+            (&Outcome::Win, &Throw::Paper) => 3,
+            (&Outcome::Win, &Throw::Scissors) => 1,
+            (&Outcome::Draw, &Throw::Rock) => 1,
+            (&Outcome::Draw, &Throw::Paper) => 2,
+            (&Outcome::Draw, &Throw::Scissors) => 3,
+            (&Outcome::Loss, &Throw::Rock) => 3,
+            (&Outcome::Loss, &Throw::Paper) => 1,
+            (&Outcome::Loss, &Throw::Scissors) => 2,
+            _ => 0,
+        }
+    }
+
+    fn total(&self) -> i32 {
+        self.compare() + self.throw_value()
     }
 }
 
-fn str_to_throw(input: &str) -> Throw {
-    match input {
-        "A" => Throw::Rock,
-        "B" => Throw::Paper,
-        "C" => Throw::Scissors,
-        "X" => Throw::Rock,
-        "Y" => Throw::Paper,
-        "Z" => Throw::Scissors,
-        _ => Throw::Error,
-    }
-}
 
-
-fn compare(throws: Vec<Throw>) -> i32 {
-    let first = &throws[1];
-    let second = &throws[0];
-    match first {
-        &Throw::Rock => {
-            match second {
-                &Throw::Rock => 3,
-                &Throw::Paper => 0,
-                &Throw::Scissors => 6,
-                _ => 0,
-            }
-        },
-        &Throw::Paper => {
-            match second {
-                &Throw::Rock => 6,
-                &Throw::Paper => 3,
-                &Throw::Scissors => 0,
-                _ => 0,
-            }
-        },
-        &Throw::Scissors => {
-            match second {
-                &Throw::Rock => 0,
-                &Throw::Paper => 6,
-                &Throw::Scissors => 3,
-                _ => 0,
-            }
-        },
-        _ => { 0 },
-    }
-}
-
-fn clean(input: &str) -> i32 {
-    let split = input.split(" ").map(|s| str_to_throw(s)).collect::<Vec<Throw>>();
-    let mut sum = throw_value(&split[1]);
-    sum += compare(split);
-    sum
+fn parse(input: &str) -> i32 {
+    let split = input.split(" ").collect::<Vec<&str>>();
+    Game::new(split).total()
 }
 
 fn main() {
     let input = read_file("input");
-    let throws = input.lines().map(|l| clean(l)).collect::<Vec<i32>>();
-    println!("Part 1: {:?}", throws.iter().sum::<i32>());
+    let throws = input.lines().map(|l| parse(l)).collect::<Vec<i32>>();
+    println!("{}", throws.iter().sum::<i32>())
 }
