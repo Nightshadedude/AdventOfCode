b0ffdbc4b48309082e915b1d4323891a8846e133
nightshadedude <nightshadedude@gmail.com>
2023-11-09 23:39:47 -0700

    part2 complete

--- 2015/erlang/day1/day1.erl
+++ 2015/erlang/day1/day1.erl
@@ -1,9 +1,13 @@
 -module(day1).
--export([part1/1]).
+-export([part1/1, part2/1]).
 
 part1(FileName) -> 
     L = readlines(FileName),
     pop_and_eval(L).
+
+part2(FileName) ->
+    L = readlines(FileName),
+    accumulate_steps({L,0,0}). %{list, sum, step}
     
 readlines(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
@@ -21,7 +25,22 @@ get_all_lines(Device) ->
 pop_and_eval([]) -> 0;
 pop_and_eval([H|T]) ->
         if
-        H == 40 -> pop_and_eval(T) + 1;
-        H == 41 -> pop_and_eval(T) + -1;
+        % '(' = 40 dec
+        % ')' = 41 dec
+        H == 40 -> pop_and_eval(T) + 1; 
+        H == 41 -> pop_and_eval(T) - 1;
         true -> 0
+    end.
+
+accumulate_steps({[], Sum, Step}) ->
+    {[], Sum, Step};
+accumulate_steps({_, -1, Step}) ->
+     Step;
+accumulate_steps({[H|T], Sum, Step}) ->
+    if
+        % '(' = 40 dec
+        % ')' = 41 dec
+        H == 40 -> accumulate_steps({T,Sum + 1, Step + 1});
+        H == 41 -> accumulate_steps({T,Sum - 1, Step + 1});
+        true -> accumulate_steps({T,Sum, Step + 1})
     end.
