7201e47ff55633008510d2a0939b4b330dc68f47
nightshadedude <nightshadedude@gmail.com>
2023-11-11 22:44:50 -0700

    day 4 part 2 complete

--- 2015/erlang/day4/day4.erl
+++ 2015/erlang/day4/day4.erl
@@ -1,5 +1,5 @@
 -module(day4).
--export([part1/0, part2/1]).
+-export([part1/0, part2/0]).
 
 
 part1() -> 
@@ -11,9 +11,10 @@ part1() ->
     iter(0, Input2),
     "Complete".
 
-part2(FileName) ->
-    FileName.
-
+part2() ->
+    Input2 = "ckczppom", %input
+    iter_2(0, Input2),
+    "Complete".
 
 md5_builder(Input, Num) ->
     Combined = io_lib:format("~s~p",[Input, Num]),
@@ -22,6 +23,10 @@ md5_builder(Input, Num) ->
 
 md5_checker(Check_Val) ->
     Binary = <<0,0,16,0,0,0,0,0,0,0,0,0,0,0,0,0>>,
+    Check_Val < Binary.
+
+md5_checker_2(Check_Val) ->
+    Binary = <<0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0>>,
     Check_Val < Binary.
 
 iter(10_000_000, Input) ->
@@ -35,4 +40,17 @@ iter(Val, Input) ->
             erlang:display(Val);
         true -> iter(Val+1, Input)
     end.
+
+iter_2(10_000_000, Input) ->
+    erlang:display(Input),
+    erlang:display("Tail reached, exiting");
+iter_2(Val, Input) ->
+    Res = md5_checker_2(md5_builder(Input, Val)),
+    if
+        Res -> 
+            erlang:display(Input),
+            erlang:display(Val);
+        true -> iter_2(Val+1, Input)
+    end.
+
 
